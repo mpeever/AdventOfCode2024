5df1b9afdebc80b4a52a82d0ee9d5a029e0f90ea
Mark Peever <mark.peever@populi.co>
2024-12-04 22:18:55 -0800

    Day 2 Part 2

--- day_2/main.go
+++ day_2/main.go
@@ -8,9 +8,13 @@ import (
 	"strings"
 )
 
+type Level int
+type Step int
+type Report []Level
+
 // check if fn evaluates to true for all elements of a list.
 // why doesn't Go come standard with decent list operators
-func all(input []int, fn func(int) bool) bool {
+func all(input []Step, fn func(Step) bool) bool {
 	if len(input) == 0 {
 		return true
 	}
@@ -24,15 +28,29 @@ func all(input []int, fn func(int) bool) bool {
 	return all(cdr, fn)
 }
 
+// Again, this should be done as part of stdlib
+func any(reports []Report, fn func(r Report) bool) bool {
+	if len(reports) == 0 {
+		return false
+	}
+	car := reports[0]
+	cdr := reports[1:]
+	if fn(car) {
+		return true
+	}
+
+	return any(cdr, fn)
+}
+
 // Delta is always b - a
-func delta(a, b int) int {
+func delta(a, b Level) Step {
 	delta := b - a
 	slog.Debug("calculating delta", "a", a, "b", b, "delta", delta)
-	return delta
+	return Step(delta)
 }
 
 // Calculate a list as a series of deltas
-func deltas(input, accumulator []int) []int {
+func deltas(input []Level, accumulator []Step) []Step {
 	if len(input) < 2 {
 		slog.Debug("returning accumlator", "accumlator", accumulator)
 		return accumulator
@@ -47,17 +65,17 @@ func deltas(input, accumulator []int) []int {
 	return deltas(cdr, accumulator)
 }
 
-func isSafe(input []int, size int) (bool, []int) {
-	acc := deltas(input, []int{})
+func isSafe(input Report, size int) (bool, []Step) {
+	acc := deltas(input, []Step{})
 
 	// first, we check that ALL of the deltas are either above or below zero
-	if !(all(acc, func(i int) bool { return i > 0 }) || all(acc, func(i int) bool { return i < 0 })) {
+	if !(all(acc, func(i Step) bool { return i > 0 }) || all(acc, func(i Step) bool { return i < 0 })) {
 		return false, acc
 	}
 
 	// OK, so all our deltas are in the same direction, now we need to find if any exceed the size limit
 	for _, d := range acc {
-		if d > size || d < (0-size) {
+		if d > Step(size) || d < Step(0-size) {
 			return false, acc
 		}
 	}
@@ -65,19 +83,58 @@ func isSafe(input []int, size int) (bool, []int) {
 	return true, acc
 }
 
-func puzzle1(input []string) (output int, err error) {
-	safetyMap := make(map[int]bool)
-	for idx, report := range input {
-		levels := []int{}
+// From a Report, generate a list of smaller Reports by removing each Level once
+func perforate(report Report) (perf []Report) {
+	for idx, level := range report {
+		slog.Debug("removing level", "idx", idx, "level", level)
+		if idx == 0 {
+			p := make([]Level, len(report)-1)
+			copy(p, report[1:])
+			perf = append(perf, p)
+		} else if idx == len(report)-1 {
+			p := make([]Level, len(report)-1)
+			copy(p, report[:idx])
+			perf = append(perf, p)
+		} else {
+			front := make([]Level, idx)
+			copy(front, report[:idx])
+			back := make([]Level, len(report)-idx-1)
+			copy(back, report[idx+1:])
+			slog.Debug("combining front and back", "front", front, "back", back)
+			p := append(front, back...)
+			perf = append(perf, p)
+		}
+	}
+	slog.Debug("perforations", "original", report, "perforations", perf)
+	return
+}
+
+// Parse input lines into a series of Reports
+func parse(input []string) (reports []Report) {
+	for _, report := range input {
+		var levels []Level
 		for _, level := range strings.Fields(report) {
 			value, err := strconv.Atoi(level)
 			if err != nil {
 				slog.Error("cannot convert String to Int", "level", level)
 				continue
 			}
-			levels = append(levels, value)
+			levels = append(levels, Level(value))
 		}
-		safe, _ := isSafe(levels, 3)
+		reports = append(reports, Report(levels))
+	}
+
+	slog.Info("parsed report count", "reports", reports, "count", len(reports))
+
+	return
+}
+
+// a report is safe if every level changes in the same direction, none have zero deltas, and no delta exceeds 3
+func puzzle1(input []string) (output int, err error) {
+	safetyMap := make(map[int]bool)
+	reports := parse(input)
+	for idx, report := range reports {
+		safe, _ := isSafe(report, 3)
 		safetyMap[idx] = safe
 	}
 
@@ -95,8 +152,26 @@ func puzzle1(input []string) (output int, err error) {
 	return
 }
 
-func puzzle2(input string) int {
-	return 0
+// a report is safe if a single level can be removed to make it safe
+func puzzle2(input []string) (safeCount int, err error) {
+	reports := parse(input)
+
+	for _, report := range reports {
+		safe, _ := isSafe(report, 3)
+		if safe {
+			safeCount++
+			continue
+		}
+
+		slog.Debug("report to perforate", "report", report)
+
+		subreports := perforate(report)
+		if any(subreports, func(r Report) bool { s, _ := isSafe(r, 3); return s }) {
+			safeCount++
+		}
+	}
+
+	return
 }
 
 func main() {
@@ -107,9 +182,18 @@ func main() {
 		line := stdioScanner.Text()
 		lines = append(lines, line)
 	}
+
+	// Puzzle 1
 	count, err := puzzle1(lines)
 	if err != nil {
 		slog.Error("puzzle1 failed", "err", err)
 	}
-	slog.Info("found safe lines", "count", count)
+	slog.Info("Puzzle 1: found safe lines", "count", count)
+
+	// Puzzle 2
+	count, err = puzzle2(lines)
+	if err != nil {
+		slog.Error("puzzle2 failed", "err", err)
+	}
+	slog.Info("Puzzle 2: found safe lines", "count", count)
 }
